0751ff21eb796b339e5e34090db72e90a41750c7
vedant130699 <vedantkulkarni1306@gmail.com>
2025-11-29 12:07:53 +0530

    configured env variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import conf from './config/config'
 
 function App() {
+  console.log(conf)
   return (
     <>
         <h1>Blog with apprwite</h1>
